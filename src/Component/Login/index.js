585f4989035bc9839dd8273c9a3dfefae000baa7
Oluwatimileyin Samuel <abegundeosamuel@gmail.com>
2022-10-11 16:45:00 +0100

    Changes

--- src/Component/Login/index.js
+++ src/Component/Login/index.js
@@ -5,6 +5,7 @@ import LoginImg from '../../Assets/Rectangle.png'
 import Flower from '../../Assets/Frame_flower.png'
 import EmailIcon from './EmailIcon'
 import PadlockIcon from './PadlockIcon'
+import { Link } from 'react-router-dom'
 export default function Login() {
     return (
         <div className='login-container'>
@@ -13,17 +14,14 @@ export default function Login() {
                 <div className='login-header'>
                     <h2>
                         Welcome back
-
                     </h2>
                     <p>
                         Login to your account
                     </p>
                 </div>
-                <img src={Flower} />
+                <img src={Flower} alt='flower'/>
             </div>
             <div className='input-container'>
-
-
                 <div className='input-container-single'>
                     <div className='input-content'>
                         <EmailIcon />
@@ -36,7 +34,16 @@ export default function Login() {
                         <PadlockIcon />
                         <input className='password_input' type='password' placeholder="......" />
                     </div>
-
+                </div>
+                <div className='forgot-pswd'>
+                    Forgot Password ?
+                </div>
+                <button type='button' className='button'>
+                    Login
+                </button>
+                <div className='account'>
+                    Don't have an account?
+                    <Link to='/signup' style={{color:'#1C8A4A', fontWeight:'600'}}> Signup</Link>
                 </div>
             </div>
 
